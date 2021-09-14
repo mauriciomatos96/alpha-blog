974bee31a79e5144c2097470ad2e91513e298d04
Mauricio <mauricio8_1996@hotmail.com>
2021-09-14 19:15:00 -0400

    added the final changes and links to modified the routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
+
     end
     
     def index
@@ -13,12 +15,11 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
-
+        
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save 
             flash[:notice] = "Article was created succesfully"
             redirect_to @article
@@ -28,8 +29,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was edited succesfully"
             redirect_to @article
         else
@@ -37,4 +37,19 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
